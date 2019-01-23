d81a3200c55145bbdd45ed4f386fff794d331ef1
Raphael Morsomme <rmorsomme@gmail.com>
2019-01-23 10:38:49 +0100

    App deployment

--- Shiny App - Poker/deploy shiny app.R
+++ Shiny App - Poker/deploy shiny app.R
@@ -3,7 +3,7 @@ library(shiny)
 library(rsconnect)
 
 # Running app on own device
-runApp("Shiny App - Poker")
+#runApp("Shiny App - Poker")
 
 # Deploying app on online server
 deployApp("Shiny App - Poker")
